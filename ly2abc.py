e5852d353da39d8c22781bed566293df82307ec6
Aaron Elkiss <aelkiss@umich.edu>
2018-03-19 23:20:14 -0400

    WIP: basic rhythm mapping
    Next:
      - ly -> abc note mapping
      - handle other stuff in note stream (repeats, ppmark,etc)

--- ly2abc.py
+++ ly2abc.py
@@ -2,7 +2,7 @@ import ly.document
 import ly.music
 from sys import argv
 
-assignments = { 
+header_fields = { 
     'title': 'T',
     'subtitle': 'T',
     'poet': 'C',
@@ -16,6 +16,12 @@ assignments = {
 def pitch_map(p):
   return p.output().capitalize()
 
+def duration_map(d):
+  eighths = d*8
+  if eighths > 1:
+    return eighths
+  else:
+    return ""
 
 def ly_globals(g):
   for k in g.find(ly.music.items.KeySignature):
@@ -26,8 +32,27 @@ def ly_globals(g):
 # def chords:
 #   print("Handling chords")
 
-def music(a):
-  print("Handling music")
+def music(music_assign):
+  time_so_far = 0
+  for m in music_assign.find(ly.music.items.MusicList,depth=2):
+    for n in music_assign.find(ly.music.items.Note):
+      # assume we're in 6/8
+
+      if(n.length() > 0):
+        print(f"{pitch_map(n.pitch)}{duration_map(n.length())}", end='')
+        time_so_far += n.length()
+#        print(f"time so far: {time_so_far}")
+        if time_so_far*8 % 6 == 0: # barline every 6 beats
+          print(" | ",end='')
+          if time_so_far*8 % 36 == 0: # newline every 6 measures / 36 beats
+            print()
+        elif time_so_far*8 % 3 == 0: # split beaming in the middle
+          print(" ",end='')
+
+  print("")
+  print("")
+        
+
 
 dispatch = {
 #    'ppTempo': tempo,
@@ -36,21 +61,16 @@ dispatch = {
     'ppMusicOne': music
 }
 
-
-def process_assignment(a):
-  abc_field = assignments[a.name()]
-  print(f"{abc_field}: {a.value().plaintext()}")
-
-
 f=open(argv[1],"r")
 d=ly.document.Document(f.read())
 m=ly.music.document(d)
 
 for h in m.find(ly.music.items.Header):
   for a in h.find(ly.music.items.Assignment):
-    process_assignment(a)
+    abc_field = header_fields[a.name()]
+    print(f"{abc_field}: {a.value().plaintext()}")
 
-for a in m.find(ly.music.items.Assignment):
+for a in m.find(ly.music.items.Assignment,depth=1):
   dispatch.get(a.name(), lambda x: None)(a)
 
 # key signature --> find in global
